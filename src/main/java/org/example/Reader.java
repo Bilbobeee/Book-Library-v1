5a2bbd80c5304a178491f2e41c3e2652bc7478e2
Bilbobeee <bilbobeee@gmail.com>
2022-10-23 22:21:05 +0300

    commit #2

---
+++ src/main/java/org/example/Reader.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Reader {
+    private int id;
+    private String name;
+
+    public Reader(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
